3a5f296115c465ebbfd37e4d70ce48893c0c43d4
Pieter Reyneke <pieter@asit4u.co.za>
2020-02-02 12:08:42 +0200

    initial commit

---
+++ app/src/test/java/com/imdbapp/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imdbapp.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
