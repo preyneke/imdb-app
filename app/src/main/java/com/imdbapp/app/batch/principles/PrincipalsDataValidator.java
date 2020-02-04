aff7e73b01691c76aef45db6e895c77a3acc86cb
Pieter Reyneke <pieter@asit4u.co.za>
2020-02-04 18:04:34 +0200

    complete data validation

--- app/src/main/java/com/imdbapp/app/batch/principles/PrincipalsDataValidator.java
+++ app/src/main/java/com/imdbapp/app/batch/principles/PrincipalsDataValidator.java
@@ -2,6 +2,7 @@ package com.imdbapp.app.batch.principles;
 
 import com.imdbapp.app.DAO.entities.Principals;
 import com.imdbapp.app.DAO.entities.PrincipalsFromFile;
+import com.imdbapp.app.constants.Constants;
 import org.springframework.batch.item.ItemProcessor;
 
 import java.util.Arrays;
@@ -12,9 +13,10 @@ public class PrincipalsDataValidator implements ItemProcessor<PrincipalsFromFile
        Principals principals = Principals.builder()
                .tconst(principalsFromFile.getTconst())
                .nconst(principalsFromFile.getNconst())
+               .ordering(principalsFromFile.getOrdering())
                .category(principalsFromFile.getCategory())
-               .job(principalsFromFile.getJob())
-               .characters(principalsFromFile.getCharacters()).build();
+               .job(!principalsFromFile.getJob().contains(Constants.NULL_REFERENCE)? principalsFromFile.getJob(): null)
+               .characters(!principalsFromFile.getCharacters().contains(Constants.NULL_REFERENCE)? principalsFromFile.getCharacters():null).build();
         System.out.println("Inserting Principle "+ principals);
         return principals;
     }
